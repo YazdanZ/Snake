f28a08d108a3f65e38ff09d7f7a670a68e7342b1
Yazdan Zinati <yazdanzin@gmail.com>
2020-11-22 20:27:41 -0500

    Add a scoreboard to the game

--- src/game.py
+++ src/game.py
@@ -1,4 +1,4 @@
-import pygame, sys,time
+import pygame, sys, time, json, os
 
 import constants
 from food import Food
@@ -10,6 +10,7 @@ class Game:
         self.start_game()
 
     def start_game(self):
+        Game.create_scores_file()
         pygame.init()
         self.clock = pygame.time.Clock()
         self.set_screen()
@@ -27,6 +28,8 @@ class Game:
                 self.snake.increment_size()
                 self.food.generate_food(self.snake)
             if self.snake.is_dead():
+                Game.update_stats(len(self.snake.body))
+                Game.print_stats(len(self.snake.body))
                 time.sleep(0.5)
                 self.start_game()
             self.update_screen()
@@ -51,6 +54,43 @@ class Game:
     def check_exit_event(event):
         if event.type == pygame.QUIT:
             sys.exit()
+
+    @staticmethod
+    def create_scores_file():
+        if not os.path.exists("stats.json"):
+            stats = {"High Score": 0, "Games Played": 0}
+            with open("stats.json", "w") as json_file:
+                json.dump(stats, json_file)
+
+    @staticmethod
+    def update_stats(score):
+        with open("stats.json", "r+") as json_file:
+            stats = json.load(json_file)
+            json_file.seek(0, 0)
+            stats["Games Played"] = int(stats["Games Played"]) + 1
+            if score > int(stats["High Score"]):
+                stats["High Score"] = score
+            json.dump(stats, json_file)
+
+    @staticmethod
+    def print_stats(score):
+        with open("stats.json", "r") as json_file:
+            stats = json.load(json_file)
+
+        scoreboard = """
+                        ---------------------------------
+                        |             stats             |
+                        ---------------------------------
+                        ---------------------------------
+                        score: {}                     
+                        high score: {}                
+                        games played: {}              
+                        ---------------------------------""".format(
+            score, stats["High Score"], stats["Games Played"]
+        )
+
+        _ = os.system("clear")
+        print(scoreboard)
 
 
 Game()
